063aa8612ac1a44aec0882fa3f33f81f1bfad1a7
shivakarka <shivakarka@gmail.com>
2023-11-09 14:09:28 +0530

    Added scroll effect

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    if (window.scrollY > 0) setActive(true);
+    else setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           {/* <Link to="/"> */}
@@ -20,11 +35,15 @@ const Navbar = () => {
           <button>Join</button>
         </div>
       </div>
-      <hr />
-      <div className="menu">
-        <span>Test</span>
-        <span>Test</span>
-      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
