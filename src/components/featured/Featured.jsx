9222cfc608b70b4cb939fea4db9ace3e480135e4
shivakarka <shivakarka@gmail.com>
2023-11-10 13:27:20 +0530

    Added Featured component

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,45 @@
+import "./Featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h1 className="title">
+            Find the right{" "}
+            <span>
+              <em>freelance</em>
+            </span>{" "}
+            service, right away
+          </h1>
+          <div className="search">
+            <input type="text" placeholder="Search for any service..." />
+            <button className="btn">
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentFill"
+              >
+                <path d="m15.89 14.653-3.793-3.794a.37.37 0 0 0-.266-.109h-.412A6.499 6.499 0 0 0 6.5 0C2.91 0 0 2.91 0 6.5a6.499 6.499 0 0 0 10.75 4.919v.412c0 .1.04.194.11.266l3.793 3.794a.375.375 0 0 0 .531 0l.707-.707a.375.375 0 0 0 0-.53ZM6.5 11.5c-2.763 0-5-2.238-5-5 0-2.763 2.237-5 5-5 2.762 0 5 2.237 5 5 0 2.762-2.238 5-5 5Z"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Web Design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src="./img/man.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
-import "./Home.scss"
+import Featured from "../../components/featured/Featured";
+import "./Home.scss";
 
 const Home = () => {
-    return (
-        <div className="home">
-        <h1>Home</h1>
-        </div>
-    );
-    }
+  return (
+    <div className="home">
+      <Featured />
+    </div>
+  );
+};
 
 export default Home;
