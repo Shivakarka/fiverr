903cad071b78d8aa7a3c0d516b9eca1db25d76f0
shivakarka <shivakarka@gmail.com>
2023-11-10 17:35:39 +0530

    Added features section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { cards } from "../../../data";
 import Slide from "../../components/Slide/Slide";
 import CatCard from "../../components/catCard/CatCard";
@@ -6,6 +7,19 @@ import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 
 const Home = () => {
+  const [show, setShow] = useState(false);
+
+  const tickImg = (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path>
+      <path d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path>
+    </svg>
+  );
   return (
     <div className="home">
       <Featured />
@@ -17,6 +31,65 @@ const Home = () => {
             <CatCard key={card.id} card={card} />
           ))}
         </Slide>
+      </div>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h2>The best part? Everything.</h2>
+            <div className="title">
+              {tickImg}
+              <h6>Stick to your budget</h6>
+            </div>
+            <p>
+              Find the right service for every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              {tickImg}
+              <h6>Get quality work done quickly</h6>
+            </div>
+            <p>
+              Hand your project over to a talented freelancer in minutes, get
+              long-lasting results.
+            </p>
+            <div className="title">
+              {tickImg}
+              <h6>Pay when you're happy</h6>
+            </div>
+            <p>
+              Upfront quotes mean no surprises. Payments only get released when
+              you approve.
+            </p>
+            <div className="title">
+              {tickImg}
+              <h6>Count on 24/7 support</h6>
+            </div>
+            <p>
+              Our round-the-clock support team is available to help anytime,
+              anywhere.
+            </p>
+          </div>
+          <div className="item" onClick={() => setShow(true)}>
+            {show ? (
+              <video
+                src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/vmvv3czyk2ifedefkau7"
+                controls
+                autoPlay
+                poster="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_700,dpr_1.0/v1/attachments/generic_asset/asset/089e3bb9352f90802ad07ad9f6a4a450-1599517407052/selling-proposition-still-1400-x1.png"
+              ></video>
+            ) : (
+              <picture>
+                <div className="play-btn">
+                  <i class="fa fa-play" style={{ fontSize: "40px" }}></i>
+                </div>
+                <img
+                  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_700,dpr_1.0/v1/attachments/generic_asset/asset/089e3bb9352f90802ad07ad9f6a4a450-1599517407052/selling-proposition-still-1400-x1.png"
+                  alt=""
+                />
+              </picture>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
