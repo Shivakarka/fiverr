611460d14a23d946b994494fecf9a10293e3d3c7
shivakarka <shivakarka@gmail.com>
2023-11-10 14:30:33 +0530

    Added TrustedBy component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
     </div>
   );
 };
