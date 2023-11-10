2df3585ea9066865a06d166a827cbc7d40d15fdc
shivakarka <shivakarka@gmail.com>
2023-11-10 16:31:01 +0530

    Added Slider using infinite-react-carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,6 @@
+import { cards } from "../../../data";
+import Slide from "../../components/Slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
@@ -7,6 +10,14 @@ const Home = () => {
     <div className="home">
       <Featured />
       <TrustedBy />
+      <div className="Slides-container">
+        <h2>Popular services</h2>
+        <Slide slidesToShow={5} arrowsScroll={5}>
+          {cards.map((card) => (
+            <CatCard key={card.id} card={card} />
+          ))}
+        </Slide>
+      </div>
     </div>
   );
 };
