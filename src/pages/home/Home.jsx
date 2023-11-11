8b1d3a8a2f31c2a8354fa944e46a7c507f15e2a4
shivakarka <shivakarka@gmail.com>
2023-11-11 23:46:00 +0530

    Added solutions banner section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,21 +5,11 @@ import CatCard from "../../components/catCard/CatCard";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
+import { checkSvg, fiverTitle, tickImg } from "../../../svgs";
 
 const Home = () => {
   const [show, setShow] = useState(false);
 
-  const tickImg = (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path>
-      <path d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path>
-    </svg>
-  );
   return (
     <div className="home">
       <Featured />
@@ -176,6 +166,64 @@ const Home = () => {
               <div className="line"></div>
               <span>Photography</span>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>
+              <span className="fiverr-title">{fiverTitle}</span>{" "}
+              <span>Business Solutions</span>
+            </h1>
+            <h2>Advanced solutions and professional talent for businesses</h2>
+            <ul>
+              <li>
+                <div>{checkSvg}</div>
+                <div className="features-content">
+                  <div>
+                    <b>Fiverr Pro</b>
+                  </div>
+                  <div>
+                    Access top freelancers and professional business tools for
+                    any project
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div>{checkSvg}</div>
+                <div className="features-content">
+                  <div>
+                    <b>Fiverr Certified</b>
+                  </div>
+                  <div>
+                    {" "}
+                    Build your own branded marketplace of certified experts
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div>{checkSvg}</div>
+                <div className="features-content">
+                  <div>
+                    <b>Fiver Enterprise</b>
+                  </div>
+                  <div>
+                    {" "}
+                    Manage your freelance workforce and onboard additional
+                    talent with an end-to-end SaaS solution
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+            <button>Learn More</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/51c35c7cecf75e6a5a0110d27909a2f5-1690202609364/EN.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
