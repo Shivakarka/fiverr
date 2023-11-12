62a25770d3576465f1584d7facb4077f1e217c66
shivakarka <shivakarka@gmail.com>
2023-11-12 23:49:35 +0530

    Add login form with input fields and submit button

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
 import "./Login.scss";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  function handleSubmit(e) {
+    console.log("handleSubmit");
+  }
+
   return (
     <div className="login">
-      <h1>Login</h1>
+      <div className="image">
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/identification_perseus/standard.ddd97e5.png"
+          alt="fiverr image"
+        />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="Enter username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && error}
+      </form>
     </div>
   );
 };
