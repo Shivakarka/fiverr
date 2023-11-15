cb4cf138165b89700636fd7a5b352cc074120761
shivakarka <shivakarka@gmail.com>
2023-11-15 18:13:44 +0530

    Add mutation for adding reviews and error handling

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,9 +1,13 @@
 import "./Reviews.scss";
 import Review from "../review/Review";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import { useState } from "react";
 
 const Reviews = ({ gigId }) => {
+  const queryClient = useQueryClient();
+  const [err, setErr] = useState("");
+
   const { isLoading, error, data } = useQuery({
     queryKey: ["reviews"],
     queryFn: () =>
@@ -12,7 +16,7 @@ const Reviews = ({ gigId }) => {
       }),
   });
 
-  if (data.length === 0) {
+  if (data?.length === 0) {
     return (
       <div className="reviews">
         <h2>Reviews</h2>
@@ -21,15 +25,51 @@ const Reviews = ({ gigId }) => {
     );
   }
 
+  const mutation = useMutation({
+    mutationFn: (review) => {
+      return newRequest.post("/reviews", review);
+    },
+    onSuccess: () => {
+      setErr("");
+      queryClient.invalidateQueries(["reviews"]);
+    },
+    onError: (error) => {
+      setErr(error?.response?.data?.error);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const desc = e.target[0].value;
+    const star = e.target[1].value;
+    mutation.mutate({ gigId, desc, star });
+  };
+
   return (
     <div className="reviews">
       <h2>Reviews</h2>
-
       {isLoading
         ? "loading"
         : error
         ? "Something went wrong!"
         : data.map((review) => <Review key={review._id} review={review} />)}
+      {err && setTimeout(() => setErr(""), 6000) && (
+        <p className="err">{err}</p>
+      )}
+      <div className="add">
+        <h3>Add a review</h3>
+        <form className="addForm" onSubmit={(e) => handleSubmit(e)}>
+          <textarea name="desc" cols="40" rows="8"></textarea>
+          <select name="star">
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
   );
 };
