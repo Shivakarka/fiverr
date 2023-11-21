d9421d6885fcd0535f0f92b54339284e8d307aaa
shivakarka <shivakarka@gmail.com>
2023-11-21 17:52:17 +0530

    Remove Sass plugin from Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sass from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sass()],
+  plugins: [react()],
 });
